bc2d0f3493b290becb8a256145c1ec4d24a88788
Aurélien <aurelien.monceau@gmail.com>
2022-06-07 23:41:15 +0200

    test ajout image localhost sur serveur

--- src/pages/CreatePostpriv.js
+++ src/pages/CreatePostpriv.js
@@ -66,7 +66,7 @@ function CreatePost() {
   
 
     axios
-      .post("https://reves7.herokuapp.com/postspriv", formData,   {
+      .post("http://localhost:3001/postspriv", formData,   {
         headers: { accessToken: localStorage.getItem("accessToken") },
       })
    
